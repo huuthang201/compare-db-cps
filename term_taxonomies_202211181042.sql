67a150cf639edb8a98c5c2ad41910d98609fe04d
Nguyen Huu Thang <64240532+huuthang201@users.noreply.github.com>
2022-11-18 17:43:09 +0700

    Add files via upload

---
+++ term_taxonomies_202211181042.sql
@@ -0,0 +1,7 @@
+INSERT INTO cps.term_taxonomies (term_id,taxonomy_id,term_type) VALUES
+	 (123,1,'posts'),
+	 (123,2,'posts'),
+	 (123,3,'posts'),
+	 (123,12,'posts'),
+	 (123,13,'posts'),
+	 (123,15,'posts');
